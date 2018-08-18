642fe85d4a1eb974a4ea170903441c05a83d03c7
Brad Garropy <bradgarropy@gmail.com>
2018-08-18 15:26:17 -0500

    favicons.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -17,6 +17,11 @@ const IndexLayout = ({children}) => {
                 <title>photogarropy</title>
                 <meta name="description" content="Sample"/>
                 <meta name="keywords" content="sample, something"/>
+                <link rel="icon" type="image/png" href="icons/favicon-196x196.png" sizes="196x196" />
+                <link rel="icon" type="image/png" href="icons/favicon-128x128.png" sizes="128x128" />
+                <link rel="icon" type="image/png" href="icons/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/png" href="icons/favicon-32x32.png" sizes="32x32" />
+                <link rel="icon" type="image/png" href="icons/favicon-16x16.png" sizes="16x16" />
             </Helmet>
 
             <Navigation/>
