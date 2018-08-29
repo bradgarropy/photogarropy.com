0535906aabc165f15187cc3f389910857aab330d
Brad Garropy <bradgarropy@gmail.com>
2018-08-28 20:52:47 -0500

    testimonials page.

--- src/pages/testimonials.jsx
+++ src/pages/testimonials.jsx
@@ -1,24 +1,33 @@
 import React from "react"
-import PropTypes from "prop-types"
+
+// styles
+import "../scss/Testimonials.scss"
 
 
-const Testimonials = (props) => {
+const Testimonials = () => {
 
     return (
 
-        <p>Testimonials</p>
+        <div className="testimonials">
+
+            <div className="testimonial">
+                <img src="images/img_8856.jpg"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.</p>
+            </div>
+
+            <div className="testimonial">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.</p>
+                <img src="images/img_8857.jpg"/>
+            </div>
+
+            <div className="testimonial">
+                <img src="images/img_8858.jpg"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.</p>
+            </div>
+
+        </div>
 
     )
-
-}
-
-
-Testimonials.propTypes = {
-
-}
-
-
-Testimonials.defaultProps = {
 
 }
 
