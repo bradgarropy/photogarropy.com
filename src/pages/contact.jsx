9980236061aa2b1ac2961f433871371db32823aa
Brad Garropy <bradgarropy@gmail.com>
2018-08-28 21:40:13 -0500

    contact page.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,24 +1,47 @@
 import React from "react"
-import PropTypes from "prop-types"
+
+// styles
+import "../scss/Contact.scss"
 
 
-const Contact = (props) => {
+const Contact = () => {
 
     return (
 
-        <p>Contact</p>
+        <div className="contact">
+
+            <div>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
+
+                <form id="contact-form" className="contact-form">
+
+                    <div className="contact-form-field">
+                        <label>Name</label>
+                        <input type="text" name="name"/>
+                    </div>
+
+                    <div className="contact-form-field">
+                        <label>Email</label>
+                        <input type="email" name="email"/>
+                    </div>
+
+                    <div className="contact-form-field">
+                        <label>Message</label>
+                        <textarea rows="4" name="message"></textarea>
+                    </div>
+
+                    <button>Submit</button>
+
+                </form>
+
+            </div>
+
+            <img src="images/img_8859.jpg"/>
+
+        </div>
 
     )
-
-}
-
-
-Contact.propTypes = {
-
-}
-
-
-Contact.defaultProps = {
 
 }
 
