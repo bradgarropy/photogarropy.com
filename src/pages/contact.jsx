1e771057edde830892f1ac175bbe31b67862a187
Brad Garropy <bradgarropy@gmail.com>
2018-08-31 14:56:30 -0500

    functional stateless contact component.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,63 +7,54 @@ import Honeypot from "../components/Honeypot"
 import "../scss/Contact.scss"
 
 
-class Contact extends React.Component {
+const Contact = () => {
 
-    onSubmit(event) {
-        event.preventDefault()
-        console.log("submitted!")
-    }
+    return (
 
-    render() {
+        <div className="container contact">
 
-        return (
+            <div>
 
-            <div className="container contact">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
 
-                <div>
+                <form
+                    id="contact-form"
+                    className="contact-form"
+                    name="contact"
+                    method="post"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    onSubmit={this.onSubmit}
+                >
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
+                    <Honeypot/>
 
-                    <form
-                        id="contact-form"
-                        className="contact-form"
-                        name="contact"
-                        method="post"
-                        data-netlify="true"
-                        data-netlify-honeypot="bot-field"
-                        onSubmit={this.onSubmit}
-                    >
+                    <div className="contact-form-field">
+                        <label>Name</label>
+                        <input type="text" name="name"/>
+                    </div>
 
-                        <Honeypot/>
+                    <div className="contact-form-field">
+                        <label>Email</label>
+                        <input type="email" name="email"/>
+                    </div>
 
-                        <div className="contact-form-field">
-                            <label>Name</label>
-                            <input type="text" name="name"/>
-                        </div>
+                    <div className="contact-form-field">
+                        <label>Message</label>
+                        <textarea rows="4" name="message"></textarea>
+                    </div>
 
-                        <div className="contact-form-field">
-                            <label>Email</label>
-                            <input type="email" name="email"/>
-                        </div>
+                    <button type="submit">Submit</button>
 
-                        <div className="contact-form-field">
-                            <label>Message</label>
-                            <textarea rows="4" name="message"></textarea>
-                        </div>
-
-                        <button type="submit">Submit</button>
-
-                    </form>
-
-                </div>
-
-                <img src="/images/img_8859.jpg"/>
+                </form>
 
             </div>
 
-        )
+            <img src="/images/img_8859.jpg"/>
 
-    }
+        </div>
+
+    )
 
 }
 
