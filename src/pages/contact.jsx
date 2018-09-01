1ff1e5835b692472795a58913284f1a037d77220
Brad Garropy <bradgarropy@gmail.com>
2018-08-31 21:54:35 -0500

    changing name.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
                 <form
                     id="contact-form"
                     className="contact-form"
-                    name="contact"
+                    name="contact-form"
                     method="post"
                     data-netlify="true"
                     data-netlify-honeypot="bot-field"
