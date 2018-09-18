8d234295a686491feb6b5a4e900abfa8d8e590c8
Brad Garropy <bradgarropy@gmail.com>
2018-09-17 22:32:40 -0500

    adding google analytics. closes #14.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
             resolve: "gatsby-plugin-react-helmet",
         },
         {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: process.env.TRACKING_ID,
+                head: true,
+                anonymize: true,
+                respectDNT: false,
+            },
+        },
+        {
             resolve: "gatsby-plugin-google-fonts",
             options: {
                 fonts: [
