d0fde4fb32041b521d1986faa010beeca8a68237
Brad Garropy <bradgarropy@gmail.com>
2018-09-21 14:35:19 -0500

    twitter meta tags. closes #19.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -19,13 +19,22 @@ const Layout = ({children}) => {
 
             <Helmet>
                 <title>photogarropy</title>
-                <meta name="description" content="📷 wife's photography business"/>
+
+                <meta name="description" content="Browse my work and book a shoot with me!"/>
                 <meta name="keywords" content="gatsby, react, markdown, scss, eslint, photography"/>
-                <link rel="icon" type="image/png" href="/icons/favicon-196x196.png" sizes="196x196" />
-                <link rel="icon" type="image/png" href="/icons/favicon-128x128.png" sizes="128x128" />
-                <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96" />
-                <link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32" />
-                <link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16" />
+
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@bradgarropy"/>
+                <meta name="twitter:creator" content="@mrsgarropy"/>
+                <meta name="twitter:title" content="📷 photogarropy"/>
+                <meta name="twitter:description" content="Browse my work and book a shoot with me!"/>
+                <meta name="twitter:image" content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto/photogarropy/img_8855.jpg"/>
+
+                <link rel="icon" type="image/png" href="/icons/favicon-196x196.png" sizes="196x196"/>
+                <link rel="icon" type="image/png" href="/icons/favicon-128x128.png" sizes="128x128"/>
+                <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96"/>
+                <link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32"/>
+                <link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16"/>
             </Helmet>
 
 
